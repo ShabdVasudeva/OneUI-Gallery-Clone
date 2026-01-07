e37b0ad36fde7e93244ef41914632c6347884b01
Ayaan Hussain <kingaah83@gmail.com>
2026-01-08 02:19:05 +0530

    Fixed videos showing after image issue
    - Also made it to show in from new->old order
    
    - Added video duration on thumbnail

--- app/src/main/java/apw/sec/android/gallery/FetchAll.kt
+++ app/src/main/java/apw/sec/android/gallery/FetchAll.kt
@@ -15,7 +15,7 @@ class FetchAll(private val context: Context) {
         mediaFiles.addAll(fetchMedia(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI, "Video"))
         mediaFiles.addAll(fetchMedia(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "Pictures"))
         mediaFiles.addAll(fetchMedia(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "Download"))
-        return mediaFiles
+        return mediaFiles.sortedByDescending { it.dateAdded ?: 0L }
     }
 
     private fun fetchMedia(
@@ -24,11 +24,25 @@ class FetchAll(private val context: Context) {
         folderName: String
     ): List<MediaFile> {
         val mediaFiles = mutableListOf<MediaFile>()
-        val projection = arrayOf(
-            MediaStore.MediaColumns._ID,
-            MediaStore.MediaColumns.DISPLAY_NAME,
-            MediaStore.MediaColumns.DATA
-        )
+        val isVideo = mediaUri == MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+
+        // Add DURATION and DATE_ADDED to projection
+        val projection = if (isVideo) {
+            arrayOf(
+                MediaStore.MediaColumns._ID,
+                MediaStore.MediaColumns.DISPLAY_NAME,
+                MediaStore.MediaColumns.DATA,
+                MediaStore.MediaColumns.DATE_ADDED,
+                MediaStore.Video.Media.DURATION
+            )
+        } else {
+            arrayOf(
+                MediaStore.MediaColumns._ID,
+                MediaStore.MediaColumns.DISPLAY_NAME,
+                MediaStore.MediaColumns.DATA,
+                MediaStore.MediaColumns.DATE_ADDED
+            )
+        }
 
         val selection = "${MediaStore.MediaColumns.DATA} LIKE ?"
         val selectionArgs = arrayOf("%$folderName%")
@@ -44,13 +58,28 @@ class FetchAll(private val context: Context) {
         cursor?.use {
             val idColumn = it.getColumnIndexOrThrow(MediaStore.MediaColumns._ID)
             val nameColumn = it.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME)
+            val dateAddedColumn = it.getColumnIndexOrThrow(MediaStore.MediaColumns.DATE_ADDED)
+            val durationColumn = if (isVideo) it.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION) else -1
 
             while (it.moveToNext()) {
                 val id = it.getLong(idColumn)
                 val name = it.getString(nameColumn)
+                val dateAdded = it.getLong(dateAddedColumn)
+                val duration = if (isVideo && durationColumn != -1) it.getLong(durationColumn) else null
                 val contentUri = Uri.withAppendedPath(mediaUri, id.toString())
-                val type = if (mediaUri == MediaStore.Images.Media.EXTERNAL_CONTENT_URI) "Image" else "Video"
-                mediaFiles.add(MediaFile(contentUri.toString(), name, type, null))
+                val type = if (isVideo) "Video" else "Image"
+
+                mediaFiles.add(
+                    MediaFile(
+                        uri = contentUri.toString(),
+                        name = name,
+                        type = type,
+                        folderName = null,
+                        groupName = null,
+                        dateAdded = dateAdded,
+                        duration = duration
+                    )
+                )
             }
         }
 
